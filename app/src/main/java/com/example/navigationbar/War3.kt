c3967c9e4311ab8be0a49d008214c8d86dc24b56
synk0120 <synk0120@naver.com>
2024-02-02 09:52:33 +0900

    buttonEdit123

--- app/src/main/java/com/example/navigationbar/War3.kt
+++ app/src/main/java/com/example/navigationbar/War3.kt
@@ -1,11 +1,24 @@
 package com.example.navigationbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class War3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_war3)
+
+        val button_back=findViewById<Button>(R.id.button_back)
+
+        fun moveToAnotherPage(className: Class<*>) {
+            val intent = Intent(this, className)
+            startActivity(intent)
+        }
+        button_back.setOnClickListener {
+            moveToAnotherPage(KnowledgeSelect::class.java)
+        }
+
     }
 }
--- app/src/main/java/com/example/navigationbar/War4.kt
+++ app/src/main/java/com/example/navigationbar/War4.kt
@@ -1,11 +1,25 @@
 package com.example.navigationbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class War4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_war4)
+
+        val button_back=findViewById<Button>(R.id.button_back)
+
+        fun moveToAnotherPage(className: Class<*>) {
+            val intent = Intent(this, className)
+            startActivity(intent)
+        }
+        button_back.setOnClickListener {
+            moveToAnotherPage(KnowledgeSelect::class.java)
+        }
+
+
     }
 }
